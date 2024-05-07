8ad4f99088e64dcd18db61e61442df3850da7200
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-05-07 17:58:58 +0300

    init commit

---
+++ src/main/java/com/example/stationmanagement/StationManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.stationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StationManagementApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/stationmanagement/StationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
